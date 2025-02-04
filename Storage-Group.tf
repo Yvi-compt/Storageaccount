a92b4f60489d2c726a3f1791b8ec027e8bc276ab
Yvi-compt <yvesplacide@gmail.com>
2025-02-03 22:33:57 -0500

    Update Storage-Group.tf

--- Storage-Group.tf
+++ Storage-Group.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "storagerg" {
 
 resource "azurerm_storage_account" "storageaccount" {
   name                     = "storageaccount-yp"
-  resource_group_name      = azurerm_resource_group.example.name
+  resource_group_name      = azurerm_resource_group.storagerg.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
