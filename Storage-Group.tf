546c94299a3e8843ad882bda1527b9a6db83daae
Yvi-compt <yvesplacide@gmail.com>
2025-02-03 19:16:19 -0500

    Update and rename Storage-Group to Storage-Group.tf

---
+++ Storage-Group.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+    feature {}
+}
+resource "azurerm_resource_group" "storagerg" {
+  name     = "Storage.rg"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "storageaccount-yp"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
