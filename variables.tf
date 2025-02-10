990ec7b8a2a6edb13aedcd09a8a4b89205125275
Yvi-compt <yvesplacide@gmail.com>
2025-02-10 15:19:48 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,3 +15,7 @@ variable "tenant_id"{
   type=string
  default = "premium" 
 }*/
+variable "mcitvm_names" {
+  type    = list(string)
+  default = ["web-server-1", "web-server-2", "web-server-3", webserver-4, webserver-5, webserver-6, webserver-7, webserver-8,webserver-9, webserver-10]
+}
