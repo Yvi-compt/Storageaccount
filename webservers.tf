9f61d9957f4fb63a3a5c765ff93781f0d6e40faf
Yvi-compt <yvesplacide@gmail.com>
2025-02-10 15:49:39 -0500

    Update webservers.tf

--- webservers.tf
+++ webservers.tf
@@ -5,6 +5,19 @@ resource "azurerm_virtual_machine" "mcityvesvm" {
   resource_group_name   = azurerm_resource_group.storagerg.name
   vm_size               = "Standard_B1s"
 
+
+resource "azurerm_network_interface" "nic" {
+  for_each            = toset(var.network_interfaces)
+  name                = each.key
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
   # Assuming you have an existing network and image
   network_interface_ids = [azurerm_network_interface.nic[count.index].id]
   storage_image_reference {
