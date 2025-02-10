9fcab6248f53d2482aeb120176da4d6ed148bb4c
Yvi-compt <yvesplacide@gmail.com>
2025-02-10 15:26:03 -0500

    Update webservers.tf

--- webservers.tf
+++ webservers.tf
@@ -1,6 +1,6 @@
-resource "azurerm_virtual_machine" "vm" {
-  count                 = length(var.vm_names)
-  name                  = var.vm_names[count.index]
+resource "azurerm_virtual_machine" "mcityvesvm" {
+  count                 = length(var.mcitvm_names)
+  name                  = var.mcitvm_names[count.index]
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   vm_size               = "Standard_B1s"
