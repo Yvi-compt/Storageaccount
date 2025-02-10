f354a3ebd53adb5c26132a29756f66c06864151d
Yvi-compt <yvesplacide@gmail.com>
2025-02-10 16:01:12 -0500

    Update Virtualnetworkyvi.tf

--- Virtualnetworkyvi.tf
+++ Virtualnetworkyvi.tf
@@ -1,13 +1,13 @@
 resource "azurerm_virtual_network" "virnet" {
   name                = "vnetwork"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.yvesmcitarg.location
-  resource_group_name = azurerm_resource_group.yvesmcitarg.name
+  location            = azurerm_resource_group.storagerg.location
+  resource_group_name = azurerm_resource_group.storagerg.name
 }
 
 resource "azurerm_subnet" "subneta" {
   name                 = "subnet-a"
-  resource_group_name  = azurerm_resource_group.yvesmcitarg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.storagerg.name
+  virtual_network_name = azurerm_virtual_network.virnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
