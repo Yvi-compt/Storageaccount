e83b799476e1298c20f8266dca581ab2a4f0c300
Yvi-compt <yvesplacide@gmail.com>
2025-02-10 20:28:11 -0500

    Update yvikubercluster.tf

--- yvikubercluster.tf
+++ yvikubercluster.tf
@@ -30,7 +30,7 @@ output "client_certificate" {
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.yvicluster.kube_config_raw
 
   sensitive = true
 }
