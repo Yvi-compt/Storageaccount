029aa48d5a6fccb7fb64a7e50543b014d1c71d3c
Yvi-compt <yvesplacide@gmail.com>
2025-02-12 17:04:43 -0500

    Update mains.tf

--- mains.tf
+++ mains.tf
@@ -1,3 +1,23 @@
 resource "azurerm_kubernetes_clusteer""yviaks"{
 name        =var.aks_cluster_name
-location    =azurer_resource_group.
+location    =azurer_resource_group.storagerg.location
+resource_group_name = azurerm_resource_group.storagerg.name
+ dns_prefix          = var.aks_cluster_name
+
+ default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+ identity {
+    type = "SystemAssigned"
+  }
+}
+resource "azurerm_kubernetes_cluster_node_pool" "aks_node_pools" {
+  for_each = var.node_pools
+
+  name                  = each.key
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.yviaks.id
+  vm_size               = each.value.vm_size
+  node_count            = each.value.node_count
+}
