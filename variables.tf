ec99329bc1aab9536e0a8833667bc27a53488647
Yvi-compt <yvesplacide@gmail.com>
2025-02-12 16:50:51 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,4 +30,35 @@ type=string
 variable "admin_password"{
 type=string
 }
-
+variable "resource_group_name" {
+  description = "Nom du groupe de ressources Azure"
+  type        = string
+  default     = "my-resource-group"
+}
+variable "location" {
+  description = "Localisation des ressources Azure"
+  type        = string
+  default     = "estus"
+}
+variable "aks_cluster_name" {
+  description = "Nom du cluster AKS"
+  type        = string
+  default     = "yvi-aks-cluster"
+}
+variable "node_pools" {
+  description = "Configuration des node pools"
+  type = map(object({
+    node_count = number
+    vm_size    = string
+  }))
+  default = {
+    "pool1" = {
+      node_count = 1
+      vm_size    = "Standard_DS2_v2"
+    },
+    "pool2" = {
+      node_count = 2
+      vm_size    = "Standard_DS3_v2"
+    }
+  }
+}
